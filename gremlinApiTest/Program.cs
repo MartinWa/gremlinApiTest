03c915238868ade3e0eac5033b136b7ef91c38c9
Martin Wåger <martin.wager@comaround.com>
2018-11-09 13:49:42 +0100

    almost working, no edges

--- gremlinApiTest/Program.cs
+++ gremlinApiTest/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Gremlin.Net.CosmosDb;
-using Gremlin.Net.CosmosDb.Structure;
-using Newtonsoft.Json;
+using Gremlin.Net.Driver.Exceptions;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Structure;
+
 
 namespace gremlinApiTest
 {
     internal class Program
     {
-        private static double TotalRuCost = 0.0;
+        private static double _totalRuCost;
 
         private static async Task Main()
         {
@@ -16,40 +20,88 @@ namespace gremlinApiTest
             using (var graphClient = new GraphClient(Secrets.Hostname, Secrets.Database, Secrets.Graph, Secrets.AuthKey))
             {
                 var g = graphClient.CreateTraversalSource();
-                var rootNode = g.AddV("node").Property("root", true).Property("depth", 0);
-                var queryString = rootNode.ToGremlinQuery();
-                HandleResponse(await graphClient.QueryAsync(rootNode));
-                for (var depth = 1; depth < 10; depth++)
+
+                // Delete all
+                var deleteQuery = g.V().Drop();
+                await DebugPrintQuery(graphClient, deleteQuery);
+
+                // Create root node
+                var rootNodeQuery = g.AddV("node").Property("root", true).Property("depth", 0);
+                await DebugPrintQuery(graphClient, rootNodeQuery);
+
+                // Create nodes
+                for (var depth = 1; depth < 3; depth++)
                 {
 
-                    var parentsQuery = g.V().Has("depth", depth-1);
-                    var response = await graphClient.QueryAsync(parentsQuery);
-                    var id = Math.Pow(10, depth);
-                    foreach (var parentResult in response.Result)
+                    var parentsQuery = g.V().Has("depth", depth - 1);
+                    var parentResults = await DebugPrintQuery(graphClient, parentsQuery);
+                    var id = Convert.ToInt32(Math.Pow(10, depth));
+                    foreach (var parentResult in parentResults)
                     {
                         var parentId = parentResult.Id;
-                        var query = g
-                                .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).AddE("child").From(parentId)
-                                .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).AddE("child").From(parentId)
-                                .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).AddE("child").From(parentId)
-                                .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).AddE("child").From(parentId)
-                                .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).AddE("child").From(parentId);
-                        queryString = query.ToGremlinQuery();
-                        HandleResponse(await graphClient.QueryAsync(query));
+                        var childrenQuery = g
+                            .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).As("1")
+                            .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).As("2")
+                            .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).As("3")
+                            .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).As("4")
+                            .AddV("node").Property("nodeId", id++).Property("Ugam", 3L).Property("depth", depth).As("5")
+                            .AddE("child").From("1").To(parentId)
+                            .AddE("child").From("2").To(parentId)
+                            .AddE("child").From("3").To(parentId)
+                            .AddE("child").From("4").To(parentId)
+                            .AddE("child").From("5").To(parentId);
+                        await DebugPrintQuery(graphClient, childrenQuery);
                     }
                 }
             }
+            Console.WriteLine($"Total RU cost: {_totalRuCost}");
+            Console.ReadLine();
         }
 
-        private static void HandleResponse(GraphResult result)
+        private static async Task DebugPrintQuery(IGraphClient graphClient, GraphTraversal<Vertex, Edge> query)
         {
-            if (result.StatusCode == 200)
+            try
             {
-                TotalRuCost += result.TotalRequestCharge;
-                return;
+                var queryString = query.ToGremlinQuery();
+                var response = await graphClient.QueryAsync(query);
+                if (response.StatusCode == 200)
+                {
+                    _totalRuCost += response.TotalRequestCharge;
+                    Console.WriteLine($"{queryString}, {response.TotalRequestCharge}");
+                    return;
+                }
+                Console.WriteLine($"Code: {response.StatusCode}");
+                Console.WriteLine($"RU Cost: {response.TotalRequestCharge}");
+                throw new HttpRequestException(response.StatusCode.ToString());
             }
-            Console.WriteLine($"Code: {result.StatusCode}");
-            Console.WriteLine($"RU Cost: {result.TotalRequestCharge}");
+            catch (ResponseException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static async Task<IReadOnlyCollection<Gremlin.Net.CosmosDb.Structure.Vertex>> DebugPrintQuery(IGraphClient graphClient, GraphTraversal<Vertex, Vertex> query)
+        {
+            try
+            {
+                var queryString = query.ToGremlinQuery();
+                var response = await graphClient.QueryAsync(query);
+                if (response.StatusCode == 200)
+                {
+                    _totalRuCost += response.TotalRequestCharge;
+                    Console.WriteLine($"{queryString}, {response.TotalRequestCharge}");
+                    return response.Result;
+                }
+                Console.WriteLine($"Code: {response.StatusCode}");
+                Console.WriteLine($"RU Cost: {response.TotalRequestCharge}");
+                throw new HttpRequestException(response.StatusCode.ToString());
+            }
+            catch (ResponseException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
